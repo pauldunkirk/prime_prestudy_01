d922f8e802a79d732467e2f80f3ada0256892c60
Paul C Dunkirk <paulcdunkirk@Pauls-MacBook-Air.local>
2017-01-09 17:07:34 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var name = prompt("Who you?");
+
+var sup = prompt("Hi " + name + "! Sup?");
+
+next = prompt(sup + "? That's sweet! What else " +name+ "?");
+
+var next = prompt(next + "? That's so goofy!");
+
+alert("Later Guv'na!");
+
+console.log("Hey! This works!");
